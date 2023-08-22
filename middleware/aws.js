cb1382c5f713adaf45b10a5fa28b06233f6d5153
him104 <himanshuchauhan696@gmail.com>
2023-08-23 01:57:09 +0530

    adding aws sdk

--- middleware/aws.js
+++ middleware/aws.js
@@ -1,8 +1,8 @@
 const aws = require("aws-sdk");
 
 aws.config.update({
-    accessKeyId: "AKIAY3L35MCRVFM24Q7U",
-    secretAccessKey: "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",
+    accessKeyId: "adding soon",
+    secretAccessKey: "adding soon",
     
   })
   
