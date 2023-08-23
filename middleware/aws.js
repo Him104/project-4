0baeaac476f075b0198b764861ec3f0238225a45
himanshu <himanshuchauhan696@gmail.com>
2023-08-23 18:49:17 +0530

    adding s3 bucket

--- middleware/aws.js
+++ middleware/aws.js
@@ -1,15 +1,16 @@
 const aws = require("aws-sdk");
 
 aws.config.update({
-    accessKeyId: "adding soon",
-    secretAccessKey: "adding soon",
+    accessKeyId: "AKIA4DY2WFBASIK46R5I",
+    secretAccessKey: "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",
+    region: "Asia Pacific (Mumbai) ap-south-1",
     
   })
   
   let uploadFile= async ( file) =>{
     return new Promise( function(resolve, reject) {
      // this function will upload file to aws and return the link
-     let s3= new aws.S3(); // we will be using the s3 service of aws
+     let s3 = new aws.S3(); // we will be using the s3 service of aws
   
      var uploadParams= {
         
