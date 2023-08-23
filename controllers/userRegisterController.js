ecf34c86d105590d1f657b26cd9fd102e11d35c7
him104 <himanshuchauhan696@gmail.com>
2023-08-24 01:34:20 +0530

    adding address of shipping

--- controllers/userRegisterController.js
+++ controllers/userRegisterController.js
@@ -2,11 +2,13 @@ const userModel = require("../models/userModel.js");
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const aws = require('../middleware/aws.js')
 
 
 const createUser = async function (req, res) {
     try {
       let data = req.body;
+      let files = req.files;
       //let profileImage = req.file;
       //data.profileImage = profileImage;
 
@@ -38,10 +40,6 @@ const createUser = async function (req, res) {
           .send({ status: false, msg: "email already exists" });
       }
 
-      if (!data.profileImage) {
-        return res.status(400).send({ status: false, msg: "profileImage is required" });
-      }
-
       if (!data.phone) {
         return res.status(400).send({ status: false, msg: "phone is required" });
       }
@@ -68,30 +66,43 @@ const createUser = async function (req, res) {
           .status(400)
           .send({
             status: false,
-            msg: "password length should be between 8 to 15 characters",
-          });
+            msg: "password length should be between 8 to 15 characters"});
       }
 
-      if (!data.address.shipping.street) {
-        return res.status(400).send({ status: false, msg: "Address Shipping street is required" });
-      }
-      if (!data.address.shipping.city) {
-        return res.status(400).send({ status: false, msg: "Address Shipping city is required" });
-      }
-      if (!data.address.shipping.pincode) {
-        return res.status(400).send({ status: false, msg: "Address Shipping pincode is required" });
-      }
+      let salt = await bcrypt.genSalt(10)
+      data.password = await bcrypt.hash(data.password,salt)
 
-      if (!data.address.billing.street) {
-        return res.status(400).send({ status: false, msg: "Address billing street is required" });
-      }
-      if (!data.address.billing.city) {
-        return res.status(400).send({ status: false, msg: "Address billing city is required" });
-      }
-      if (!data.address.billing.pincode) {
-        return res.status(400).send({ status: false, msg: "Address billing pincode is required" });
-      }
-  
+      if (data.address) {
+
+        let addressValidation = data.address
+        
+      
+
+      if (!addressValidation.shipping.street) 
+        return res.status(400).send({ status: false, msg: "shipping street is required" });
+      
+      if(!addressValidation.shipping.city)
+      return res.status(400).send({ status: false, msg: "Shipping city is required" })
+
+      if(!addressValidation.shipping.pincode)
+      return res.status(400).send({ status: false, msg: "Shipping pincode is required" })
+    if(!addressValidation.billing.street)
+    return res.status(400).send({ status: false, msg: "billing street is required" })
+    if(!addressValidation.billing.city)
+    return res.status(400).send({ status: false, msg: "billing city is required" })
+
+    if(!addressValidation.billing.pincode)
+    return res.status(400).send({ status: false, msg: "billing pincode is required" })
+      
+      if (files && files.length>0) {
+       
+        let uploadedFileURL= await aws.uploadFile( files[0] )
+        data.profileImage = uploadedFileURL
+    }
+    else{
+        res.status(400).send({ msg: "No file found" })
+    }
+          
       let createdUser = await userModel.create(data);
       res
         .status(201)
@@ -100,9 +111,10 @@ const createUser = async function (req, res) {
           message: "User created successfully",
           data: createdUser,
         });
+      }
     } catch (error) {
       return res.status(500).send({ msg: error.message });
     }
-  };
+  }
 
   module.exports.createUser = createUser;
--- middleware/aws.js
+++ middleware/aws.js
@@ -3,26 +3,28 @@ const aws = require("aws-sdk");
 aws.config.update({
     accessKeyId: "AKIA4DY2WFBASIK46R5I",
     secretAccessKey: "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",
-    region: "Asia Pacific (Mumbai) ap-south-1",
+    region: "ap-south-1",
     
   })
   
   let uploadFile= async ( file) =>{
     return new Promise( function(resolve, reject) {
      // this function will upload file to aws and return the link
-     let s3 = new aws.S3(); // we will be using the s3 service of aws
+     let s3 = new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
   
      var uploadParams= {
-        
+        ACL:'public-read',
          Bucket: "him104-aws",  
          Key: "abc/" + file.originalname, 
-         Body: "uploading images"
+         Body: file.buffer
      }
   
      s3.upload( uploadParams, function (err, data ){
          if(err) {
              return reject({"error": err})
          }
+         console.log(data);
+         console.log("file uploaded successfully");
           return resolve(data.Location)
      })
   
