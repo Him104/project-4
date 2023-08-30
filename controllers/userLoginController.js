84a031464a3cfb18a6c136cfebf4febf298cb395
Subhash Mourya <subhash@euronics.com>
2023-08-30 15:54:21 +0530

    added Cat POST API

--- controllers/userLoginController.js
+++ controllers/userLoginController.js
@@ -1,4 +1,4 @@
-const userModel = require("../models/userModel.js");
+const userModel = require("../models/userModel.js");      
 const jwt = require('jsonwebtoken'); 
 const bcrypt = require('bcrypt');
 
--- models/productModel.js
+++ models/productModel.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose"); 
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({ 
 
   title: {type: String, required: true, unique: true, trim: true},
   description: {type: String,required: true,trim: true},
   price: {type: Number, required: true, trim: true },
   currencyId: {type: String,required: true,trim: true},
-  currencyFormat: {type: String, required: true, trim: true },
+  currencyFormat: {type: String, trim: true, default: "₹"},
   isFreeShipping: {type: Boolean, default: false},
 
   productImage: {type: String,required: true,trim: true},
