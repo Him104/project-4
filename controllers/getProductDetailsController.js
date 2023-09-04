1205344cc74ea90a2ff64389dc44d2d1e6e1c713
Subhash Mourya <subhash@euronics.com>
2023-09-04 19:53:34 +0530

    fix add to cart and get cart

--- controllers/getProductDetailsController.js
+++ controllers/getProductDetailsController.js
@@ -1,71 +1,85 @@
 const productModel = require("../models/productModel.js");
 const mongoose = require("mongoose");
 
-const getProductDetails = async function(req,res){
-    
-    try {
 
-const query = req.query;
+const getProduct = async function (req, res) {
+  try {
+    const requestQuery = req.query
+    const { size, name, priceGreaterThan, priceLessThan, priceSort } = requestQuery
+    const filterQuery = { isDeleted: false }
 
-if (Object.keys(query).length==0) {
+    if (Object.keys(requestQuery).length == 0) {
 
-const allProducts = await productModel.find({isDeleted:false}).sort({ title: 'asc' });
+      const findProducts = await productModel.find({ isDeleted: false }).sort({ title: 'asc' });
 
 
-if (allProducts.length !=0 ) {
+      if (findProducts.length != 0) {
 
-return res.status(200).send({status:true,data:allProducts})
+        return res.status(200).send({ status: true, count: findProducts.length, data: findProducts })
 
-}
-
-}
-
-if (Object.keys(query).length!=0) {
-
-  query.isDeleted = false;
-  const getByQuery = await productModel.find(query).sort({ title: 'asc' });
-
-       if(getByQuery.length !=0){
-        return res.status(200).send({status:true , data:getByQuery})
       }
 
-      if (getByQuery.length ==0){
-        return  res.status(404).send({ status: false, msg: "No Product found by filter"});
+    }
+
+    if (Object.keys(requestQuery).length > 0) {
+      if (size) {
+        let size1 = size.split(",").map(x => x.trim().toUpperCase())
+        //if (size1.map(x => isValidSize(x)).filter(x => x === false).length !== 0) return res.status(400).send({ status: false, message: "Size Should be among  S,XS,M,X,L,XXL,XL" })
+        filterQuery.availableSizes = { $in: size1 }
       }
 
+      if (name) {
+        let findTitle = await productModel.find()
+        let fTitle = findTitle.map(x => x.title).filter(x => x.includes(name))
 
-}
+        if (fTitle.length == 0) { filterQuery.title = name }
+        filterQuery.title = { $in: fTitle }
+      }
 
-  
-} catch (error) {
-  res.status(500).send({status:false, error:message.error})
-   
-}
+      if (priceSort) {
+        if (priceSort != 1 && priceSort != -1) return res.status(400).send({ status: false, message: "priceSort should be among 1 and -1." })
+      }
+
+      if (priceGreaterThan && priceLessThan) { filterQuery.price = { $gt: priceGreaterThan, $lt: priceLessThan } }
+      if (priceGreaterThan && !priceLessThan) { filterQuery.price = { $gt: priceGreaterThan } }
+      if (priceLessThan && !priceGreaterThan) { filterQuery.price = { $lt: priceLessThan } }
+    }
+
+    const findProducts = await productModel.find(filterQuery).sort({ price: priceSort })
+
+    if (findProducts.length == 0) return res.status(404).send({ status: false, message: "products not found or may be deleted" })
+
+    return res.status(200).send({ status: true, count: findProducts.length, message: "products details", data: findProducts })
+  }
+  catch (err) {
+    return res.status(500).send({ status: false, error: err.message })
+  }
 }
 
 
 const getProductDetailsById = async function (req, res) {
-    try {
-      let productId = req.params.productId;
-      
-      
-      
-      const productDetails = await productModel.findById(productId);
-      if (!productDetails) { 
-        
-        return res
-          .status(400)
-          .send({ status: false, msg: "No productDetails Found!" });
-      }
+  try {
+    let productId = req.params.productId;
+
+
+
+    const productDetails = await productModel.findById(productId);
+    if (!productDetails) {
 
       return res
-        .status(200)
-        .send({ status: true, msg: "Product Details", data:productDetails});
-     
-    } catch (error) {
-      res.status(500).send({ status: false, error: error.msg });
+        .status(400)
+        .send({ status: false, msg: "No productDetails Found!" });
     }
-  };
-  
-  module.exports.getProductDetails = getProductDetails;
-  module.exports.getProductDetailsById = getProductDetailsById;
+
+    return res
+      .status(200)
+      .send({ status: true, msg: "Product Details", data: productDetails });
+
+  } catch (error) {
+    res.status(500).send({ status: false, error: error.msg });
+  }
+};
+
+module.exports.getProduct = getProduct;
+//module.exports.getProductDetails = getProductDetails;
+module.exports.getProductDetailsById = getProductDetailsById;
